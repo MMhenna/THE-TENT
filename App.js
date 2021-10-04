741a6b7dfaba62785b40ca59a598be61a38d9a17
MMhenna <mhenna.mehdi@gmail.com>
2021-10-04 15:39:50 +0100

    commit

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import firebase from 'firebase';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyD1a0YdjrIndEIMKuAdBlQy6itaMYuJGWE",
+
+  authDomain: "the-tent-5a04f.firebaseapp.com",
+
+  projectId: "the-tent-5a04f",
+
+  storageBucket: "the-tent-5a04f.appspot.com",
+
+  messagingSenderId: "598600014797",
+
+  appId: "1:598600014797:web:c5fc05ee5108901468c284",
+
+  measurementId: "G-BSBGDCLC8Z"
+
+};
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register';
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initiaRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
