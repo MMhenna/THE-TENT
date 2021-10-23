92379479e8e7f09d3f1dcd0311c3a3380987d37b
MMhenna <mhenna.mehdi@gmail.com>
2021-10-23 16:43:08 +0100

    commit

--- components/main/Add.js
+++ components/main/Add.js
@@ -11,56 +11,124 @@ import { AppStyles } from "../../AppStyles";
 
 export default function Add() {
   return (
-    <View>
-      <View >
-          <TextInput style={styles.body}
-            placeholder="Place"
-            onChangeText={() => this.setState({  })}
-            placeholderTextColor={AppStyles.color.grey}
-            underlineColorAndroid="transparent"
-            />
+    <View style={styles.container}>
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          placeholder="Place"
+          onChangeText={() => this.setState({})}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
       </View>
-      <View >
-          <TextInput style={styles.body}
-            placeholder=""
-            onChangeText={() => this.setState({  })}
-            placeholderTextColor={AppStyles.color.grey}
-            underlineColorAndroid="transparent"
-            />
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          placeholder=""
+          onChangeText={() => this.setState({})}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
       </View>
-      <View >
-          <TextInput style={styles.body}
-            placeholder="Night "
-            keyboardType='numeric'
-            onChangeText={() => this.setState({  })}
-            placeholderTextColor={AppStyles.color.grey}
-            underlineColorAndroid="transparent"
-            />
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          placeholder="Night "
+          keyboardType="numeric"
+          onChangeText={() => this.setState({})}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
       </View>
-      <View >
-          <TextInput style={styles.body}
-            placeholder="Place"
-            onChangeText={() => this.setState({  })}
-            placeholderTextColor={AppStyles.color.grey}
-            underlineColorAndroid="transparent"
-            />
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.body}
+          placeholder="Place"
+          onChangeText={() => this.setState({})}
+          placeholderTextColor={AppStyles.color.grey}
+          underlineColorAndroid="transparent"
+        />
       </View>
-      <TouchableOpacity
-        
-        
-      >
-        <Text>add</Text>
+      <TouchableOpacity style={styles.addContainer}>
+        <Text style={styles.addText} >add</Text>
       </TouchableOpacity>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
-    body: {
-        height: 42,
-        paddingLeft: 20,
-        paddingRight: 20,
-        color: AppStyles.color.text,
-      }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  or: {
+    color: "black",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: AppStyles.fontSize.title,
+    fontWeight: "bold",
+    color: AppStyles.color.tint,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  leftTitle: {
+    alignSelf: "stretch",
+    textAlign: "left",
+    marginLeft: 20,
+  },
+  content: {
+    paddingLeft: 50,
+    paddingRight: 50,
+    textAlign: "center",
+    fontSize: AppStyles.fontSize.content,
+    color: AppStyles.color.text,
+  },
+  addContainer: {
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.tint,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30,
+    textAlign: "center",
+  },
+  addText: {
+    color: AppStyles.color.white,
+  },
+  placeholder: {
+    color: "red",
+  },
+  InputContainer: {
+    width: AppStyles.textInputWidth.main,
+    marginTop: 30,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: AppStyles.color.grey,
+    borderRadius: AppStyles.borderRadius.main,
+  },
+  body: {
+    height: 42,
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: AppStyles.color.text,
+  },
+  facebookContainer: {
+    width: 192,
+    backgroundColor: AppStyles.color.facebook,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30,
+  },
+  facebookText: {
+    color: AppStyles.color.white,
+  },
+  googleContainer: {
+    width: 192,
+    height: 48,
+    marginTop: 30,
+  },
+  googleText: {
+    color: AppStyles.color.white,
+  },
+});
