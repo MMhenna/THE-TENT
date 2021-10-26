270cd8a799d63131ac3c2d144076d163a6082b6d
ameurbesbes <ameurbesbes@outlook.com>
2021-10-26 16:03:23 +0100

    THE-TENT

--- components/Main.js
+++ components/Main.js
@@ -14,6 +14,7 @@ import AddScreen from "./main/Add";
 import ProfileScreen from "./main/Profile";
 import cardPost from "./main/cardPost";
 import Search from "./main/Search"
+import firebase  from 'firebase'
 
 const Tab = createBottomTabNavigator();
 
--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect }from 'react'
-import { StyleSheet, View, Text, Image , FlatList } from 'react-native'
+import { StyleSheet, View, Text, Image , FlatList, Button } from 'react-native'
 
 import firebase  from 'firebase'
 require('firebase/firestore')
@@ -47,6 +47,10 @@ function Profile(props) {
         }
     }, [props.route.params.uid])
 
+    const onLogout = () => {
+        firebase.auth().signOut();
+    }
+
     if(user === null){
         return <View/>
     }
@@ -55,6 +59,10 @@ function Profile(props) {
             <View styles={styles.containerInfo}> 
                 <Text>{user.name}</Text>
                 <Text>{user.email}</Text>
+                <Button
+                    title="Logout"
+                    onPress={() => onLogout()}
+                    />
             </View>
 
             <View styles={styles.containerGallery}>
