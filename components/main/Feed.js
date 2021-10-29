6db534868bc476f5f0232cbdeb90367a1e12b475
ameurbesbes <ameurbesbes@outlook.com>
2021-10-29 09:58:21 +0100

    THE TENT

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,81 +1,118 @@
-import React,{ useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import cardPost from "./cardPost";
+import React, { useState, useEffect } from "react";
+import { View, Text,Image,StyleSheet } from "react-native";
+import {Container,Card,PostImg} from "../styles/cardPostStyles"
 
-import {
-    Card,
-    CardTitle,
-    CardContent,
-    CardAction,
-    CardButton,
-    CardImage,
-} from "react-native-material-cards";
 
 
 import firebase from "firebase";
- require("firebase/firestore");
- require("firebase/firebase-storage")
+require("firebase/firestore");
+require("firebase/firebase-storage")
 
+
+// async function allpost() {
+//   const listofposts = []
+//   const dataoftheuser = []
+//   const db = firebase.firestore()
+//   const posts = await db.collection('posts').onSnapshot((val => {
+//     val.forEach(document => {
+//       const post_data = []
+//       post_data.push({ ...document.data(), key: document.id })
+//       post_data.forEach(async elem => {
+
+//         const user = await db.collection('users').doc(elem.userid).onSnapshot((data => {
+//           dataoftheuser.push({ userdata: data.data(), postdata: elem })
+//           listofposts.push(dataoftheuser)
+//         }
+//         ))
+//       })
+//     })
+//   }));
+//   console.log(listofposts)
+
+// };
+function datafinder() {
+  return [1, 3]
+}
+async function finder() {
+
+
+}
 export default function Feed() {
-  const [data, setData] = useState({});
+  const [userowners, setUserPosts] = useState([]);
 
-  useEffect(() => {
-    const fetchdata = async () => {
-      const postsRef= firebase.firestore().collection("posts");
-      const snapshot =  await postsRef.get();
-      console.log(snapshot)
-      snapshot.forEach((doc) => {
-        console.log(doc.id, "=>", doc.data());})
+    const [ posts, setPosts ] = useState([]);
+    useEffect(() => {
+      firebase.firestore()
+        .collection('posts')
+        .onSnapshot((snapshot) => setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+    }, []);
+    console.log(posts)
 
+  // const [data, setData] = useState();
+  // useEffect( () => {
+  //   const dataoftheuser = []
+  //   const listofposts = []
+  //           db.collection('posts').onSnapshot((val => {
+  //       val.forEach(document => {
+  //         const post_data = []
+  //         post_data.push({ ...document.data(), key: document.id })
+  //         post_data.forEach( elem => {
 
-      setData(snapshot);
-    };
+  //           const user =  db.collection('users').doc(elem.userid).onSnapshot((data => {
+  //             dataoftheuser.push({ userdata: data.data(), postdata: elem })
+  //             listofposts.push(dataoftheuser)
+  //           }
+  //           ))
+  //         })
+  //       })
+  //     }))
+  //     setData(dataoftheuser)
 
-    fetchdata();
-    // eslint-disable-next-line
-  }, [])
-  const arr = [];
-  for (const x in data) {
-    arr.push(data[x]);
+  //   },[])
+
+  // eslint-disable-next-line
+  // console.log(data)
+  return (<View>
+
+    {/* {data.forEach(val => {
+      val
+    })} */}
+    {posts.map((post) =>       (<Card>
+ ù     <Text  style={styles.title}>{post.id}</Text>
+      <PostImg source={post.data.downloadURL}
+ style={styles.stretch}       />
+      <Text  style={styles.PostText} >     {post.data.night}</Text>
+      <Text    >     {post.data.desc}</Text >
+      <Text   style={styles.text} >     {post.data.night}</Text>
+      <Text   style={styles.text} >     {post.data.place}</Text>
+      <Text   style={styles.text} >     {post.data.userid}</Text>
+
+ 
+    </Card>)
+
+           //post.data.creation
+          //  desc={a.data.desc}
+          //  image= {a.data.downloadURL}
+          //  night = {a.data.night}
+          //  place={a.data.place}
+          //  startDate={a.data.startDate}
+          //  userid={a.data.userid}
+          //  id ={a.id}
+    )}
+  </View >
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  stretch: {
+    width: 100,
+    height: 100,
+    resizeMode: 'stretch',
+  },
+  text:{
 
   }
-
-  console.log(arr)
-
-
-
-
-
-
-
-  return <View>{arr.map((user) => {user.hf})}
-  <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>Feed</Text>
-      </View>
-      <FlatList/>
-    </View>
-        
-      </View>
-}
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#EFECF4",
-    },
-    header: {
-      paddingTop: 64,
-      paddingBottom: 64,
-      backgroundColor: "#FFF",
-      alignItems: "center",
-      justifyContent: "center",
-      borderBottomWidth: 1,
-      borderBottomColor: "#EBECF4",
-      shadowColor: "#454D65",
-      shadowOffset: { height: 5 },
-      shadowRadius: 15,
-      shadowOpacity: 0.2,
-      zIndex: 10,
-    },
-  });
-
+});
